d7eecb2b51268fd07c5cd983c21b05c8693828a1
DivyaDiv530 <divyadivakaran530@gmail.com>
2022-06-24 19:55:59 +0530

    formValidation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NgModel } from '@angular/forms';
 import { ApiServices } from './api.service';
 
 @Component({
@@ -12,8 +13,15 @@ export class AppComponent implements OnInit {
   searchResults: any;
   userDataSelected = false;
   selectedInput: any;
+  user :{
+  firstname:string;
+  age:string;
+  }
+  @ViewChild("inputElem") inputElem: NgModel;
+  @ViewChild("f") formD: NgModel;
   constructor(public apiService: ApiServices) {}
   ngOnInit() {
+    this.user=Object.assign({});
     this.apiService.getUserData().subscribe((data) => {
       console.log('data', data);
       this.userData = data;
@@ -24,25 +32,30 @@ export class AppComponent implements OnInit {
       return user.name.toLowerCase().includes(event.target.value.toLowerCase());
     });
   }
-  toggle(userData?:any) {
-    if(userData) {
+  toggle(userData?: any) {
+    if (userData) {
       this.searchedUser = userData.name;
-    this.userDataSelected = false;
-    return;
-    }
-    else {
-    this.searchResults = this.userData;
-    this.userDataSelected = true;
+      this.userDataSelected = false;
+      return;
+    } else {
+      this.searchResults = this.userData;
+      this.userDataSelected = true;
     }
   }
   onBlur() {
-    this.userDataSelected = false;
+    setTimeout(() => {
+      this.userDataSelected = false;
+    }, 1000);
   }
   selectUserData(event) {
     //this.selectedInput=event;
     console.log('e', event);
     this.searchedUser = event.name;
     this.userDataSelected = false;
-    
+  }
+
+  onFormSubmit(f) {
+    console.log("form",f);
+    console.log("name",this.user.firstname);
   }
 }
